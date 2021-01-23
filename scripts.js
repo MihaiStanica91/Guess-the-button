407fffab7eda0632aab61cee9f658744472e8c7a
Mihai <mihai.s.9@gmail.com>
2021-01-23 15:41:31 +0000

    Final version for N buttons

--- scripts.js
+++ scripts.js
@@ -1,44 +1,26 @@
+function winnerButton() {
 
-function correctButton() {
-  alert("CORRECT!!! I am the WINNER one!!!");
-}
-
-function wrongButton() {
-    alert("WRONG!!! I am the LOSER one!!!");
+  var x = document.getElementById("myNumber").value;
+  var winnerNumber = Math.floor(Math.random() * x);
+  var div = document.getElementsByTagName("div")[0];
+  
+  for (var i = 0; i < x; i++) {
+    var button = document.createElement("button");
+    button.innerHTML = 'Press me,<br> I am the winner one';
+    div.appendChild(button);
+    button.value = i;
+    if(button.value == winnerNumber) {
+      button.addEventListener ("click", function() {
+        alert("CORRECT!!! I am the WINNER one!!!");
+      });
+    } else {
+      button.addEventListener ("click", function() {
+        alert("WRONG!!! I am the LOSER one!!!");
+      });
+    }
   }
-
-function shuffle(array) {
-    var currentIndex = array.length
-    var temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
 }
-var arr = [0, 0, 1];
 
-function rightWrong() {
-    shuffle(arr);
-    for(var i = 0; i < 3; i++) {
-      if(arr[i] == 0) {
-        var x = document.getElementById("button");
-        wrongButton(x);
-        break;
-      } else {
-          var x = document.getElementById("button");
-          correctButton(x);
-          break;
-      }
-    }
+function restartGame() {
+  location.reload();
 }
-console.log(arr);
